e9557527cc440ee3e75d318f8aecdad0b24dbb4c
William Kennedy <bill@ardanlabs.com>
2020-06-30 15:49:14 -0400

    day2.4

---
+++ app/admin/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/pkg/errors"
+)
+
+func main() {
+	// if err := keyGen(); err != nil {
+	// 	log.Println(err)
+	// 	os.Exit(1)
+	// }
+
+	if err := gentoken(); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
+func gentoken() error {
+	privatePEM, err := ioutil.ReadFile("private.pem")
+	if err != nil {
+		return err
+	}
+
+	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privatePEM)
+	if err != nil {
+		return err
+	}
+
+	// iss (issuer): Issuer of the JWT
+	// sub (subject): Subject of the JWT (the user)
+	// aud (audience): Recipient for which the JWT is intended
+	// exp (expiration time): Time after which the JWT expires
+	// nbf (not before time): Time before which the JWT must not be accepted for processing
+	// iat (issued at time): Time at which the JWT was issued; can be used to determine age of the JWT
+	// jti (JWT ID): Unique identifier; can be used to prevent the JWT from being replayed (allows a token to be used only once)
+	claims := struct {
+		jwt.StandardClaims
+		Roles []string
+	}{
+		StandardClaims: jwt.StandardClaims{
+			Issuer:    "service project",
+			Subject:   "12345",
+			ExpiresAt: time.Now().Add(8760 * time.Hour).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		Roles: []string{"ADMIN"},
+	}
+
+	method := jwt.GetSigningMethod("RS256")
+
+	tkn := jwt.NewWithClaims(method, claims)
+	str, err := tkn.SignedString(privateKey)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("-------  BEGIN TOKEN -------")
+	fmt.Println(str)
+	fmt.Println("-------  END TOKEN -------")
+
+	return nil
+}
+
+func keyGen() error {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return err
+	}
+
+	privateFile, err := os.Create("private.pem")
+	if err != nil {
+		return errors.Wrap(err, "creating private file")
+	}
+	defer privateFile.Close()
+
+	privateBlock := pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	}
+
+	if err := pem.Encode(privateFile, &privateBlock); err != nil {
+		return errors.Wrap(err, "encoding to private file")
+	}
+
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return errors.Wrap(err, "marshaling public key")
+	}
+
+	publicFile, err := os.Create("public.pem")
+	if err != nil {
+		return errors.Wrap(err, "creating public file")
+	}
+	defer privateFile.Close()
+
+	publicBlock := pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	if err := pem.Encode(publicFile, &publicBlock); err != nil {
+		return errors.Wrap(err, "encoding to public file")
+	}
+
+	return nil
+}
